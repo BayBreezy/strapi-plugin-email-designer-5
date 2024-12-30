159ee5efa72e922cab71682deb0523293eed70e4
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-12-29 21:41:04 -0500

    chore: make admin routes separate from content api routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,62 +1,89 @@
-export default [
-  {
-    method: "GET",
-    path: "/templates",
-    handler: "designer.getTemplates",
-    config: { policies: [], auth: false },
+export default {
+  // Admin dashboard only routes
+  admin: {
+    type: "admin",
+    routes: [
+      {
+        method: "GET",
+        path: "/templates",
+        handler: "designer.getTemplates",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "GET",
+        path: "/templates/:templateId",
+        handler: "designer.getTemplate",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "POST",
+        path: "/templates/:templateId",
+        handler: "designer.saveTemplate",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "DELETE",
+        path: "/templates/:templateId",
+        handler: "designer.deleteTemplate",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "POST",
+        path: "/templates/duplicate/:sourceTemplateId",
+        handler: "designer.duplicateTemplate",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "GET",
+        path: "/config/:configKey",
+        handler: "config.getConfig",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "GET",
+        path: "/config",
+        handler: "config.getFullConfig",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "GET",
+        path: "/core/:coreEmailType",
+        handler: "designer.getCoreEmailType",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "POST",
+        path: "/core/:coreEmailType",
+        handler: "designer.saveCoreEmailType",
+        config: { policies: [], auth: false },
+      },
+      {
+        method: "GET",
+        path: "/download/:id",
+        handler: "designer.download",
+        config: { policies: [], auth: false },
+      },
+    ],
   },
-  {
-    method: "GET",
-    path: "/templates/:templateId",
-    handler: "designer.getTemplate",
-    config: { policies: [], auth: false },
+  // The content api routes
+  "content-api": {
+    type: "content-api",
+    routes: [
+      {
+        method: "GET",
+        path: "/templates",
+        handler: "designer.getTemplates",
+      },
+      {
+        method: "GET",
+        path: "/templates/:templateId",
+        handler: "designer.getTemplate",
+      },
+      {
+        method: "GET",
+        path: "/download/:id",
+        handler: "designer.download",
+      },
+    ],
   },
-  {
-    method: "POST",
-    path: "/templates/:templateId",
-    handler: "designer.saveTemplate",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "DELETE",
-    path: "/templates/:templateId",
-    handler: "designer.deleteTemplate",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "POST",
-    path: "/templates/duplicate/:sourceTemplateId",
-    handler: "designer.duplicateTemplate",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "GET",
-    path: "/config/:configKey",
-    handler: "config.getConfig",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "GET",
-    path: "/config",
-    handler: "config.getFullConfig",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "GET",
-    path: "/core/:coreEmailType",
-    handler: "designer.getCoreEmailType",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "POST",
-    path: "/core/:coreEmailType",
-    handler: "designer.saveCoreEmailType",
-    config: { policies: [], auth: false },
-  },
-  {
-    method: "GET",
-    path: "/download/:id",
-    handler: "designer.download",
-    config: { policies: [], auth: false },
-  },
-];
+};
